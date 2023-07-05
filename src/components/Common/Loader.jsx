3799584fcbc5d4c260355129946347c4f5818f66
anees8 <aneesahmed8484@gmail.com>
2023-07-05 15:57:20 +0530

    Loader  changes

--- src/components/Common/Loader.jsx
+++ src/components/Common/Loader.jsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React from 'react'
 import { Backdrop, CircularProgress } from "@mui/material";
-export const loader = (props) => {
+const loader = (props) => {
   return (
-    <Backdrop
-      sx={{
-        color: "#1976d2",
-        zIndex: (theme) => theme.zIndex.drawer + 1,
-        background: "rgba(25,118,210,0.1)"
-      }}
-      open={props.loading}
-    >
-      <CircularProgress color="inherit" />
-    </Backdrop>
-  );
-};
+
+        <Backdrop
+          sx={{ color: "#1976d2", zIndex: (theme) => theme.zIndex.drawer + 1 ,background: "rgba(25,118,210,0.1)" }}
+          open={props.loading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+  )
+}
+
+export default loader
