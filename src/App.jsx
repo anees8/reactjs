277a93d1344c711d95a208af0a50a5b6a16d30d8
anees8 <aneesahmed8484@gmail.com>
2023-07-05 19:23:28 +0530

    050720233

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,19 @@ import { Route, Routes, useLocation } from "react-router";
 import { useNavigate } from "react-router-dom";
 
 import { useSelector } from "react-redux";
-import {Box} from "@mui/material";
-import VISITER_ROUTES from './routes/userRoute'
+import { Box } from "@mui/material";
+import VISITER_ROUTES from './routes/userRoute';
+import Login from "./components/Login/index";
+import Custom404 from "./components/Common/Custom404";
 
-// import Users from "./components/Users/index";
- import Login from "./components/Login/index";
-// import Employee from "./components/Employee/index";
-// import Product from "./components/Product/index";
 import Loader from "./components/Common/Loader";
 import Sidebar from "./components/Common/Sidebar/Sidebar";
-import DrawerHeader from './components/Common/Sidebar/DrawerHeader'
-
+import DrawerHeader from './components/Common/Sidebar/DrawerHeader';
 
 const App = () => {
   const location = useLocation();
   const hideNavbarRoutes = ["/", "/register"]; // List of routes where the navbar should be hidden
-  const hideNavbar = hideNavbarRoutes.includes(location.pathname);
+  const hideNavbar = hideNavbarRoutes.includes(location.pathname) || !VISITER_ROUTES.some(({ path }) => path === location.pathname) ;
   const navigate = useNavigate();
   const storedToken = localStorage.getItem("token");
   const loading = useSelector((state) => state.login.loading);
@@ -36,23 +33,24 @@ const App = () => {
   return (
     <>
       {!hideNavbar && (
-       
-          <Box sx={{ display: "flex" }}>
-            <Sidebar />
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-              <DrawerHeader />
-              <Routes>
-                {VISITER_ROUTES.map(({path,Component},index) => (
-                <Route path={path} element={<Component/>} key={index} />
+        <Box sx={{ display: "flex" }}>
+          <Sidebar  />
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <DrawerHeader />
+            <Routes>
+              {VISITER_ROUTES.map(({ path, Component }, index) => (
+                <Route path={path} element={<Component />} key={index} />
+              ))}
 
-                ))}
-              </Routes>
-            </Box>
+            </Routes>
           </Box>
+        </Box>
       )}
+
       {hideNavbar && (
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="*" element={<Custom404 />} />
         </Routes>
       )}
     </>
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,14 +1,16 @@
-import { USERS_ROUTE,LOGIN_ROUTE,PRODUCTS_ROUTE,EMPLOYEES_ROUTE } from "../config/constant/routerpathContant";
+import { USERS_ROUTE,LOGIN_ROUTE,PRODUCTS_ROUTE,EMPLOYEES_ROUTE,CUSTOM404_ROUTE } from "../config/constant/routerpathContant";
 import LOGINPAGE from "../components/Login/index";
 import USERSPAGE from "../components/Users/index";
 import PRODUCTSPAGE from "../components/Product/index";
 import EMPLOYEESPAGE from "../components/Employee/index";
+import CUSTOM404PAGE from "../components/Common/Custom404";
 
 const VISITER_ROUTES=[
 {path:LOGIN_ROUTE,Component:LOGINPAGE},
 {path:USERS_ROUTE,Component:USERSPAGE},
 {path:PRODUCTS_ROUTE,Component:PRODUCTSPAGE},
 {path:EMPLOYEES_ROUTE,Component:EMPLOYEESPAGE},
+{path:CUSTOM404_ROUTE,Component:CUSTOM404PAGE},
 ];
 
 export default VISITER_ROUTES;
